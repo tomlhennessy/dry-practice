0d38f4a0c03a941160346b87210d911b3c28ce22
Tom Hennessy <tomlhennessy@outlook.com>
2024-05-16 20:31:19 +0100

    code refactored

--- practice.js
+++ practice.js
@@ -1,45 +1,27 @@
+function getBiggerNum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+function getSmallerNum(num1, num2) {
+  return num1 < num2 ? num1 : num2;
+}
+
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  return getBiggerNum(num1, num2) * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return getBiggerNum(num1, num2) / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  const bigNum = getBiggerNum(sum1, sum2);
+  return `I ate ${bigNum} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  const smallDog = getSmallerNum(weight1, weight2);
+  return `I adopted a dog that weighs ${smallDog} pounds.`;
 }
 
 
